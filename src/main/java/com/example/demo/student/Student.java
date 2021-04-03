166770daefd890a2fdb88fb6af18ecaa53324ce9
ulyanovskk <mr1english4@gmail.com>
2021-04-03 17:03:33 +0400

    Initial commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,84 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private long id;
+    private String name;
+    private String email;
+    private int age;
+    private LocalDate dateOfBirth;
+
+    public Student() {
+    }
+
+    public Student(long id,
+                   String name,
+                   String email,
+                   int age,
+                   LocalDate dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Student(String name, String email, int age, LocalDate dateOfBirth) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
