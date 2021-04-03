2b69fa2eec8a9b46fb71b1604e14e6e20b93cf35
ulyanovskk <mr1english4@gmail.com>
2021-04-03 18:11:46 +0400

    Added: StudentRepository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,14 +10,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L,
-                        "Adapter",
-                        "bestadapter@gmail.com",
-                        20,
-                        LocalDate.of(2000, Month.MAY, 22)
-                )
-        );
+        return studentRepository.findAll();
     }
 }
